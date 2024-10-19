7ebdceced784231a557cc7864f2f4e023d61c8c0
kevin.berger <kevin.berger@mcs.ch>
2024-10-19 05:12:38 +0200

    delete unused stuff

--- src/SelfExtendingBackend/SelfExtendingBackend.Frontend/Models/ApiInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SelfExtendingBackend.Frontend.Models;
-
-public class ApiInfo
-{
-    public string Name { get; set; }
-    public string Route { get; set; }
-    public string Description { get; set; }
-    public string HttpMethod { get; set; } // e.g., GET, POST
-}
--- src/SelfExtendingBackend/SelfExtendingBackend.Frontend/Models/ProcessStep.cs
+++
@@ -1,8 +0,0 @@
-namespace SelfExtendingBackend.Frontend.Models;
-
-public class ProcessStep
-{
-    public string Name { get; set; }
-    public string Status { get; set; } // "ready", "failed", "in progress", "waiting"
-}
-
